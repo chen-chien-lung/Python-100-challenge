daaabd52d300b31a2d409c29f0d56a1f6d40c4a4
Joe Chen <chenjianlong@chenjianlongdeMacBook-Pro.local>
2020-01-21 21:59:05 +0800

    first commit

---
+++ 2.py
@@ -0,0 +1,18 @@
+"""
+Question:
+Write a program which can compute the factorial of a given numbers.
+The results should be printed in a comma-separated sequence on a single line.
+Suppose the following input is supplied to the program:
+8
+Then, the output should be:
+40320
+"""
+
+def fact(input):
+    if input == 0:
+        return 1
+    else:
+        return input * fact(input-1)
+
+x = int(input())
+print(fact(x))
---
+++ 5.py
@@ -0,0 +1,24 @@
+
+# Question:
+# Define a class which has at least two methods:
+# getString: to get a string from console input
+# printString: to print the string in upper case.
+# Also please include simple test function to test the class methods.
+#
+# Hints:
+# Use __init__ method to construct some parameters
+
+class InputOutStr(object):
+
+    def __init__(self):
+        self.s = ""
+
+    def getString(self):
+        self.s = input()
+
+    def printString(self):
+        print(self.s.upper())
+
+strObg = InputOutStr()
+strObg.getString()
+strObg.printString()
